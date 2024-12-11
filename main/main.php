72469e49ab070bdce8ca6fe8b4108e8209dca3ee
yuricynical <johnlloydbasister@gmail.com>
2024-12-11 15:43:39 +0800

    home page

---
+++ main/main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/main.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar">
+        <img height="50" src="../assets/images/CNlogo.png" class="logo" alt="">
+        <ul class="links-container">
+            <li class="link-item"><a href="/" class="link">home</a></li>
+            <li class="link-item"><a href="/" class="link">Festival</a></li>
+            <li class="link-item"><a href="/" class="link">Post</a></li>
+        </ul>
+    </nav>
+
+    <header class="header">
+        <div class="content">
+            <h1 class="heading">
+                <span class="small">Welcome to</span>
+                Camarines Norte Home of Colorful
+                <span class="no-fill">Festivals</span>
+            </h1>
+            <a href="/festivals" class="btn">View Festivals</a>
+        </div>
+    </header>
+</body>
+</html>
