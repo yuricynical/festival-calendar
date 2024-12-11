f07a1658f34a9bf51d6921e58d2294a2e37b0ce2
yuricynical <johnlloydbasister@gmail.com>
2024-12-11 22:54:52 +0800

    festivals page

--- main/home.php
+++ main/home.php
@@ -26,5 +26,83 @@
             <a href="/festivals" class="btn">View Festivals</a>
         </div>
     </header>
+
+    <section class="blogs-section">
+        <div class="blog-card">
+            <img src="../assets/images/Pabirik.jpg" class="blog-image" alt="">
+            <h1 class="blog-title">Pabirik Festival</h1>
+            <p class="blog-overview">Pabirik Festival is a festival that recognizes the gold mining
+                 in the territory of Camarines Norte. This festival obliges the 400th year Foundation 
+                 Anniversary of the town's Patron Saint which is the Our Lady of Candelaria. </p>
+                 <a href="/" class="btn dark">Read</a>
+        </div>
+
+        <div class="blog-card">
+            <img src="../assets/images/Pinyasan.jpg" class="blog-image" alt="">
+            <h1 class="blog-title">Pinyasan Festival</h1>
+            <p class="blog-overview">The Pinyasan or Pineapple Festival is celebrated yearly in the town of Daet, 
+                Camarines Norte in the Bicol Region. The festival is celebrated from June 15 to 24 which coincides
+                with the feast of the town’s patron saint, Saint John the Baptist.  </p>
+                 <a href="/" class="btn dark">Read</a>
+        </div>
+
+        <div class="blog-card">
+            <img src="../assets/images/Kadagatan.jpg" class="blog-image" alt="">
+            <h1 class="blog-title">Kadagatan Festival</h1>
+            <p class="blog-overview">The Mercedes Kadagatan Festival is a fishermen’s holiday celebration held in Mercedes, 
+                Camarines Norte, every August 6-11, in which said festivity is dedicated as a time of annual thanksgiving for 
+                the blessings and bounty received from the Almighty during the seafaring, fishing expeditions and fish farming 
+                endeavors.</p>
+                 <a href="/" class="btn dark">Read</a>
+        </div>
+
+        <div class="blog-card">
+            <img src="../assets/images/Rahugan.jpg" class="blog-image" alt="">
+            <h1 class="blog-title">Rahugan Festival</h1>
+            <p class="blog-overview">Rahugan Festival is a cultural festival, coconut festival, founding anniversary, and religious 
+                fiesta in Basud, Camarines Norte, Philippines every October 24. A week-long event, it is held in conjunction with the 
+                feast of St. Raphael the Archangel, the patron of the town.</p>
+                 <a href="/" class="btn dark">Read</a>
+        </div>
+
+        <div class="blog-card">
+            <img src="../assets/images/Busig-on.jpg" class="blog-image" alt="">
+            <h1 class="blog-title">Busig-on Festival</h1>
+            <p class="blog-overview">Celebrated every September 7-8 in Labo, Camarines Norte, this festival is based on the epic of Busig-on
+                 of Labo which exemplified the town’s historical values and sentiments through talents and skills competition featuring the 
+                 town’s places of interest.</p>
+                 <a href="/" class="btn dark">Read</a>
+        </div>
+
+        <div class="blog-card">
+            <img src="../assets/images/Mananap.jpg" class="blog-image" alt="">
+            <h1 class="blog-title">Mananap Festival</h1>
+            <p class="blog-overview">The Mananap Festival is celebrated annually from April 23 to April 29 in San Vicente, Camarines Norte. 
+                It is a prominent cultural and tourism event that highlights the local traditions, including the livelihood activities of fishing 
+                and farming. The festival features vibrant street dances, parades, and various activities that honor the town's heritage and natural 
+                abundance.</p>
+                 <a href="/" class="btn dark">Read</a>
+        </div>
+
+        <div class="blog-card">
+            <img src="../assets/images/Mambulawan.jpg" class="blog-image" alt="">
+            <h1 class="blog-title">Mambulawan Festival</h1>
+            <p class="blog-overview">Held every September 24-October 7 in Jose Panganiban, Camarines Norte to coincide with the Feast of Our Lady of 
+                the Most Holy Rosary, the festival aims to forge unity and cooperation among the local community, promote culture and arts, revitalization 
+                of mining industry, exposition of the town’s best, homecoming and involvement of Balikbayans, all geared towards advancement and economic
+                growth.</p>
+                 <a href="/" class="btn dark">Read</a>
+        </div>
+
+        <div class="blog-card">
+            <img src="../assets/images/Palong.jpg" class="blog-image" alt="">
+            <h1 class="blog-title">Palong Festival</h1>
+            <p class="blog-overview">Celebrated in Capalonga, Camarines Norte every May 10-13 of the year, this festival is highlighted by street dancing and 
+                agro-industrial fair as an expression of gratitude by the local folks. The palong (rooster comb) signifies the abundant presence of rooster in 
+                the locality. This festival is also celebrated together with the Feast Day of the Black Nazarene.</p>
+                 <a href="/" class="btn dark">Read</a>
+        </div>
+
+    </section>
 </body>
 </html>
